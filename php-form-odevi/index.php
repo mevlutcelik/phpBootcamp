baf1f48bebdf40fd349d6deacb463b66ba0523ce
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-04-16 17:46:08 +0300

    Update index.php

--- php-form-odevi/index.php
+++ php-form-odevi/index.php
@@ -6,13 +6,70 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Form Ödevi</title>
+    <style>
+        * {
+            font-family: system-ui, sans-serif
+        }
+
+        body {
+            margin: 2rem;
+        }
+
+        input {
+            background: initial;
+            border: 1px solid rgba(0, 0, 0, 0.1);
+            font-size: 0.875rem;
+            padding: 0.5rem 0.75rem;
+            outline: 0;
+            margin-left: 1rem;
+            border-radius: 0.5rem;
+        }
+
+        button {
+            border: 0;
+            outline: 0;
+            cursor: pointer;
+            background-color: #1a73e8;
+            color: #fff;
+            padding: 0.5rem 0.75rem;
+            border-radius: 0.5rem;
+            margin-left: 0.5rem;
+        }
+    </style>
 </head>
 
 <body>
+
+    <?php
+    function message($str = "", $type = "message")
+    {
+    ?>
+        <div style="color: #fff;padding: 1rem;margin-bottom: 1rem;border-radius: 0.5rem;font-family: system-ui, sans-serif;background-color:<?php echo $type === "error" ? "#f44336" : "#1a73e8"; ?>"><?php echo $str; ?></div>
+    <?php
+    }
+    if (isset($_POST["number"])) {
+        $number = @$_POST["number"];
+        if ($number === "") {
+            message("Lütfen boş bırakmayın!", "error");
+        } else {
+            if ($number % 3 === 0) {
+                message("${number} sayısı 3 ile tam bölünüyor");
+            } else {
+                $afterNum = $number;
+                $afterNum++;
+                while ($afterNum % 3 !== 0) {
+                    $afterNum++;
+                }
+                message("${number} sayısı 3 ile tam bölünmüyor. 3 ile ilk bölünecek sayı: ${afterNum}");
+            }
+        }
+    }
+    ?>
+
     <form action="" method="post">
-        <label for="number">Number</label>
-        <input type="number" name="number" placeholder="Number">
-        <button type="submit">Sent</button>
+        <label for="number">Sayı girin</label>
+        <input type="number" name="number" <?php if(isset($_POST["number"])){echo 'value="'. $_POST["number"].'"';} ?> placeholder="Sayı girin...">
+        <button type="submit">Gönder</button>
     </form>
 </body>
 
@@ -20,7 +77,7 @@
 
 <?php
 
-function check($value)
+/*function check($value)
 {
     $firstValue = $value;
     if ($value == "") {
@@ -38,6 +95,7 @@ function check($value)
     }
     return $result;
 }
-
-echo check($_POST['number']);
+if(isset($_POST)){
+    check(@$_POST['number']);
+}*/
 ?>
